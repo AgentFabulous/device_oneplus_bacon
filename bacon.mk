9e5e3e131197dadc9b6b1e97497a2eba829e8a6d
Sultanxda <sultanxda@gmail.com>
2017-01-28 16:19:46 -0800

    bacon: Use OSS camera HAL

--- bacon.mk
+++ bacon.mk
@@ -97,6 +97,10 @@ PRODUCT_PROPERTY_OVERRIDES +=
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8974
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
